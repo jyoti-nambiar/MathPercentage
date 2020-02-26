5f54a8772b0406fc69d44ffffc13ac0fd5108d73
jyoti-nambiar <50999333+jyoti-nambiar@users.noreply.github.com>
2020-02-26 14:24:52 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ document.querySelector("button").addEventListener("click",randomNumber);
 
 function randomNumber(){
     var randomnum=Math.round(Math.random()*100);
-document.querySelector("h1").innerHTML="You are "+randomnum+" % perfect for each other";
+document.querySelector("h1").innerHTML="You are "+randomnum+"% perfect for each other";
 
     return randomnum;
 
